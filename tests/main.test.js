84ef31ee921f45acb3e9389600ea4031d1886355
repetitioestmaterstudiorum <thotan@ik.me>
2022-09-02 10:34:15 +0200

    formatting only

--- tests/main.test.js
+++ tests/main.test.js
@@ -1,20 +1,20 @@
-import assert from "assert";
+import assert from 'assert'
 
-describe("rest-api-post-printer", function () {
-  it("package.json has correct name", async function () {
-    const { name } = await import("../package.json");
-    assert.strictEqual(name, "rest-api-post-printer");
-  });
+describe('rest-api-printer', function () {
+	it('package.json has correct name', async function () {
+		const { name } = await import('../package.json')
+		assert.strictEqual(name, 'rest-api-printer')
+	})
 
-  if (Meteor.isClient) {
-    it("client is not server", function () {
-      assert.strictEqual(Meteor.isServer, false);
-    });
-  }
+	if (Meteor.isClient) {
+		it('client is not server', function () {
+			assert.strictEqual(Meteor.isServer, false)
+		})
+	}
 
-  if (Meteor.isServer) {
-    it("server is not client", function () {
-      assert.strictEqual(Meteor.isClient, false);
-    });
-  }
-});
+	if (Meteor.isServer) {
+		it('server is not client', function () {
+			assert.strictEqual(Meteor.isClient, false)
+		})
+	}
+})
